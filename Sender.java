49891b8ed19a9a4810b6900a51508f0813bcebfd
siwe9840 <72326150+siwe9840@users.noreply.github.com>
2020-10-30 16:51:32 -0400

    Update Sender.java

--- Sender.java
+++ Sender.java
@@ -1,2 +1,38 @@
+import java.io.*;
+import java.util.*; 
+import java.net.*; 
+
 public class Sender{
+  
+  public static void main(String[] args) {
+    
+    String iP = args[0];
+    int senderPort = Integer.parseInt(args[1]);
+    int receiverPort = Integer.parseInt(args[2]);
+    String fileName = args[3];
+    int maxSize = Integer.parseInt(args[4]);
+    int timeout = Integer.parseInt(args[5]); 
+    
+    byte[] buffer = new byte[1024];
+		DatagramPacket packet = new DatagramPacket(buffer, 1024);
+		DatagramSocket socket = new DatagramSocket(null);
+    socket.bind(new InetSocketAddress(iP, senderPort));
+    socket.setSoTimeout(timeout); 
+    
+    //read file
+    StringBuilder stringBuilder = new StringBuilder(); 
+    Scanner scanner = new Scanner (new File(fileName));
+    while (scanner.hasNextLine()){
+      stringBuilder.append(scanner.nextLine());
+      stringBuilder.append("\n");
+    }
+    scanner.close();
+    
+   String fileData = stringBuilder.toString();
+    
+    
+    
+  }
+  
+  
 }
