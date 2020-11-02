b9442cd5e8c43a3071b8f3f3e052b6efaabe7fc6
tdchambers <50087924+tdchambers@users.noreply.github.com>
2020-11-02 10:52:16 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -9,46 +9,78 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.SwingWorker;
 
 public class Gui extends JFrame{
-
-	private static final long serialVersionUID = 1L;
+	/*--------------------------------
+	  Have NOT tested with the other files 
+		  
+	  ----------------------------------------*/
+	//  private final SocketClient socketClient;
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 		
 			
-	//Labels
-	private JLabel Port_label = new JLabel("Port #: ", SwingConstants.LEFT);
-		
-	public JLabel IP_label = new JLabel("IP Address: ", SwingConstants.LEFT);
-	public JLabel ACKPORTlabel = new JLabel("Send ACK port#: ", SwingConstants.LEFT);
-	public JLabel RECDATAlabel = new JLabel("Receive Data port#: ", SwingConstants.LEFT);
-	public JLabel FILETOWRITElabel = new JLabel("file to write: "); 
-	public JLabel NUMPACKETSlabel = new JLabel("# of received in-order packets");
+		//Labels
+		private final Receiver receiver = new Receiver();
 		
 		
 		
-	//Buttons
-	public static JButton receiveButton = new JButton("RECEIVE");
-	public static JButton Clear= new JButton("CLEAR");
-	public static JButton Connect= new JButton("Connect");
-	public static JButton Disconnect= new JButton("Disconnect ");
+		
+		private JLabel Port_label = new JLabel("Port #: ", SwingConstants.LEFT);
+		
+
+		
+		
+		
+		//Buttons
+		public static JButton receiveButton = new JButton("RECEIVE");
+		public static JButton Clear= new JButton("CLEAR");
+		public static JButton Connect= new JButton("Connect");
+		public static JButton Disconnect= new JButton("Disconnect ");
+
+
+		
+		//TextFields
+		
+		public JLabel IP_label = new JLabel("IP Address: ", SwingConstants.LEFT);
+		
+		public static JTextField IPtxt = new JTextField();
+		
+		public JLabel ACKPORTlabel = new JLabel("Send ACK port#: ", SwingConstants.LEFT);
+
+		public static JTextField ACKPORTtxt  = new JTextField();
+
+		public JLabel RECDATAlabel = new JLabel("Receive Data port#: ", SwingConstants.LEFT);
+
+		public static JTextField RECDATAtxt = new JTextField();
+
+		public JLabel FILETOWRITElabel = new JLabel("file to write: "); 
+		
 	
-	//TextFields	
-	public static JTextField IPtxt = new JTextField();
-	public static JTextField ACKPORTtxt  = new JTextField();
-	public static JTextField RECDATAtxt = new JTextField();
-	public static JTextField FILETOWRITEtxt = new JTextField();
-	public static JTextField  NUMPACKETStxt = new JTextField("0"); 
+		public static JTextField FILETOWRITEtxt = new JTextField();
+		
+		
+		
+		public JLabel NUMPACKETSlabel = new JLabel("# of received in-order packets");
+		
+		public static JTextField  NUMPACKETStxt = new JTextField("0"); 
 		
 	  
 		
-	public void addElements() {
+		public void addElements() {
 		receiveButton.setBackground(Color.GREEN);
 		receiveButton.setOpaque(true);
 		
+		
 		this.add(IP_label);
 		this.add(IPtxt);
-		
 		this.add(ACKPORTlabel);
+
+
+
 		this.add(ACKPORTtxt);
 
 		this.add(RECDATAlabel);
@@ -68,7 +100,24 @@ public class Gui extends JFrame{
 		}
 		
 		public void SubmitButtonHandler(ActionEvent e) {
-			
+			 String address = IPtxt.getText();
+             int senderPort = Integer.parseInt(ACKPORTtxt.getText());
+             int receiverPort = Integer.parseInt(RECDATAtxt.getText());
+             String outputFileName = FILETOWRITEtxt.getText();
+            // boolean reliable = checkboxReliable.isSelected();
+             
+             new SwingWorker<Void, Void>() {
+                 @Override
+                 public Void doInBackground() {
+                     try {
+                       //  receiver.setInOrderPacketLabel(NUMPACKETStxt); // Assign our JLabel to our ReceiverHandler to update it
+                         receiver.Receiving(address, senderPort, receiverPort, outputFileName);
+                     } catch (IOException exception) {
+                         exception.printStackTrace();
+                     }
+                     return null;
+                 }
+             }.execute();
 		}	
 		
 		public void ClearButtonHandler(ActionEvent e) {
