f62caf23e2294076b0a168079aa3b85d83efecb5
tdchambers <50087924+tdchambers@users.noreply.github.com>
2020-11-02 22:05:15 -0500

    Add files via upload

---
+++ miyan.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class miyan{
+			public static void main(String[] args){
+			
+				Gui F = new Gui();
+				F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+				 F.layoutView();
+				 F.setSize(400,300);
+				 F.setVisible(true);
+			}
+		}127
